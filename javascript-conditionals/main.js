6608bdc1837f16218d5eec125c0fcb6a93f10ea6
Dennis Nguyen <dennis.nguyen9925@gmail.com>
2023-07-12 14:59:25 -0700

    Updated comparison operators on line 62.

--- javascript-conditionals/main.js
+++ javascript-conditionals/main.js
@@ -59,7 +59,7 @@ function isOldEnoughToDrinkAndDrive(person){
 function categorizeAcidity(pH){
     if (pH === 7){
         return 'neutral';
-    } else if (pH === 0 && pH <= 7) {
+    } else if (pH >= 0 && pH < 7) {
         return 'acid';
     } else if (pH > 7 && pH <= 14) {
         return 'base';
