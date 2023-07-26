19d028b78e8f75891d9ab686bd341c00ef3012d5
Dennis Nguyen <dennis.nguyen9925@gmail.com>
2023-07-26 16:06:58 +0000

    Updated stylesheet and main to reflect changes requested.

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -1,20 +1,16 @@
 const $openButton = document.querySelector('.open-btn');
 const $closeButton = document.querySelector('.close-btn');
 
-$openButton.addEventListener('click', function (event) {
-  const $modal = document.querySelector('.modal');
-  const $modalContainer = document.querySelector('.modal-container');
+const $modal = document.querySelector('.modal');
+const $modalContainer = document.querySelector('.modal-container');
 
+$openButton.addEventListener('click', function (event) {
   $modal.className = $modal.className.replace('hide', '');
 
   $modalContainer.className = $modalContainer.className.replace('hide', '');
 });
 
 $closeButton.addEventListener('click', function (event) {
-  const $modal = document.querySelector('.modal');
-  const $modalContainer = document.querySelector('.modal-container');
-
-  console.log($modalContainer);
   $modal.className += 'hide';
   $modalContainer.className += 'hide';
 });
