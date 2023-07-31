ca2dc5e9b9a880d767f4255750e49af0774fb4d9
Dennis Nguyen <dennis.nguyen9925@gmail.com>
2023-07-31 03:24:18 +0000

    Complete Javascript typing tutor challenge.

---
+++ javascript-typing-tutor/main.js
@@ -0,0 +1,27 @@
+const $spans = document.querySelectorAll('.span');
+let current = 0;
+$spans[current].classList.add('curr');
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === $spans[current].textContent) {
+    $spans[current].classList.remove('curr');
+    $spans[current].classList.add('correct');
+    if ($spans[current].classList.contains('wrong')) {
+      $spans[current].classList.remove('wrong');
+    }
+    $spans[++current].classList.add('curr');
+  } else {
+    $spans[current].classList.add('wrong');
+  }
+  if (current >= $spans.length) {
+    reset();
+  }
+});
+
+function reset() {
+  current = 0;
+  $spans[current].classList.add('curr');
+  for (const span of $spans) {
+    span.classList.remove('correct');
+  }
+}
