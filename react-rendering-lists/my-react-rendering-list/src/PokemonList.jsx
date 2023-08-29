517f5a709a37c684ac9bc6025f77bb49ee3488b5
Dennis Nguyen <dennis.nguyen9925@gmail.com>
2023-08-29 22:30:28 +0000

    Updated to make list a prop. Moved Pokemon array to App and passed it as a prop to PokemonList component.

--- react-rendering-lists/my-react-rendering-list/src/PokemonList.jsx
+++ react-rendering-lists/my-react-rendering-list/src/PokemonList.jsx
@@ -1,12 +1,4 @@
-const pokedex = [
-  { number: '001', name: 'Bulbasaur' },
-  { number: '004', name: 'Charmander' },
-  { number: '007', name: 'Squirtle' },
-  { number: '025', name: 'Pikachu' },
-  { number: '039', name: 'Jigglypuff' },
-];
-
-export default function PokemonList() {
+export default function PokemonList({ pokedex }) {
   const pokemonList = pokedex.map((pokemon) => (
     <li key={pokemon.number}>{pokemon.name}</li>
   ));
